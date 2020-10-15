d66b793a6f32d785f4f74ed6b41932df612f9ee3
ReconAlpha2552 <mirko1869@hotmail.it>
2020-10-15 13:41:59 +0100

    Minigioco Cabajo
    Minigioco del Cabajo finito

---
+++ Cavajo platform/Assets/Script/Jump.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public bool Ij;
+    public int Health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        Health = 1;
+        Ij = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Ij == false) {
+
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 6, 0);
+
+            Ij = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        Ij = false;
+
+
+        if (collision.gameObject.name == "GameOverZone1"|| collision.gameObject.name == "GameOverZone2") {
+            Health = Health - 1; 
+        }
+
+        if (Health <= 0)
+        {
+            Time.timeScale = 0;
+
+        }  
+        
+        if (collision.gameObject.name == "Win") {
+            Time.timeScale = 0;
+        }
+
+    }
+
+  
+}

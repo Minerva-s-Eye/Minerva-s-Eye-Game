693b5a8871c2cc6e8134f861e9cc94b622184c0d
ReconAlpha2552 <mirko1869@hotmail.it>
2020-10-18 20:03:07 +0100

    Final Boss
    Il boss finale!

---
+++ Final Boss/Assets/Script/MoveUpAndDown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpAndDown : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed = 5f;
+
+    [SerializeField]
+    float height = 0.5f;
+
+    Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+    void Update()
+    {
+
+
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}

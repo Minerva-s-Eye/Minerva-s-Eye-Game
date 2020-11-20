6dca825fd6a6ad71e69c5bcbd129873078a13692
ReconAlpha2552 <mirko1869@hotmail.it>
2020-11-20 20:16:24 +0100

    Lupo

---
+++ Lupo/Assets/Script/Boss.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss: MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+
+
+    public Health healthBar;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+    
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+           
+        
+    
+    public void TakeDamage(int damage)
+        {
+
+            currentHealth -= 1;
+
+            healthBar.SetHealth(currentHealth);
+
+        }
+        
+    
+}
---
+++ Lupo/Assets/Script/Pg.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pg : MonoBehaviour
+{
+
+    public int  maxHealth = 10000;
+    public int currentHealth;
+    public int vitaPersa = 1;
+
+
+    public Health healthBar;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+        currentHealth -= vitaPersa;
+        healthBar.SetHealth(currentHealth);
+
+    }
+
+
+
+    public void Heal(int damage)
+    {
+
+        currentHealth += 500;
+
+        healthBar.SetHealth(currentHealth);
+
+    }
+
+
+}
